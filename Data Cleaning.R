a356e33fd26469befeab541b05a87f74d96bc0d1
AnnaWolfram <anna.wolfram@rwth-aachen.de>
2023-06-21 12:55:59 +0200

    DataCleaning

---
+++ Data Cleaning.R
@@ -0,0 +1,31 @@
+# Data Cleaning
+
+# Pakete laden
+
+#install.packages("readxl")
+
+library(tidyverse)
+library(psych)
+library(readxl)
+
+
+# Daten einlesen
+
+
+rawText <- read_xlsx("Daten/TEST_Daten.xlsx")
+rawZahl <- read_xlsx("Daten/TEST_Daten_Num.xlsx")
+
+
+# Zeilen entfernen
+
+# Spalten entfernen
+
+# Spalten umbenennen
+
+# Richtige Datentypen zuordnen
+
+# Qualitätskontrolle 
+
+# Skalen berechnen
+
+# Lösung abspeichern
