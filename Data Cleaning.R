f7c40ad3073d3786860df4a5ca55072ba298bffe
Dana Scholz <danascholz@155-030.eduroam.rwth-aachen.de>
2023-06-23 15:54:09 +0200

    R-Read

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -7,18 +7,21 @@
 library(tidyverse)
 library(psych)
 library(readxl)
-
+source("qualtricshelpers.R")
 
 # Daten einlesen
 
-
-rawZahl <- read_xlsx("Daten/Rohdaten_23.06.xlsx")
+Rohdaten <- "Daten/CSV.Rohdaten_23.06.csv"
+raw <- load_qualtrics_csv(Rohdaten)
 
 
 # Zeilen entfernen
 
+raw <- filter(raw, Progress >= 99)
+
 # Spalten entfernen
 
+raw.short <- raw[,c(-1:-5, -7:-8, -10:-17, -110)]
 # Spalten umbenennen
 
 # Richtige Datentypen zuordnen
