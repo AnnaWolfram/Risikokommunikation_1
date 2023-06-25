366716ab309aa27c0fe6e249f6c76ed229403726
Dana Scholz <danascholz@172-021.eduroam.rwth-aachen.de>
2023-06-25 16:38:12 +0200

    Blabla

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -41,11 +41,48 @@ raw.short[176,]$age = "24"
 
 raw.short$age <- as.numeric(raw.short$age)
 
+raw.short$gender[is.na(raw.short$gender)] <- 4
+
 raw.short$gender %>% 
-  recode(`1`= "männlich", `2` = "weiblich", `3`="divers") %>% 
+  recode(`1`= "männlich", `2` = "weiblich", `3`="divers", `4`="keine Angabe") %>% 
   as.factor() -> raw.short$gender
 
-raw.short$gender
+raw.short$education[is.na(raw.short$education)] <- 6
+raw.short$education %>%
+  recode(`1`="(noch) kein Schulabschluss",
+         `2`="Hauptschulabschluss / Volksschulabschluss",
+         `3`= "Realschulabschluss (Mittlere Reife)",
+         `4`="(Fach-)Abitur",
+         `5`="(Fach-)Hochschulabschluss",
+         `6`="keine Angabe")
+
+raw.short$occupation[is.na(raw.short$occupation)] <- 10
+raw.short$occupation%>%
+  recode(`1`="Schüler:in",
+         `2`="Auszubildende/-r",
+         `3`= "Student:in",
+         `4`="Voll berufstätig",
+         `5`="Teilweise berufstätig",
+         `6`="Rentner:in, Pensionär:in",
+         `7`="Arbeitslos / arbeitssuchend",
+         `8`= "Hausmann / Hausfrau und / oder versorge Kinder / pflegebedürftige Personen",
+         `9`= "Sonstiges",
+         `10`="Keine Angabe")
+
+
+
+raw.short$branch %>% 
+  recode(`1`="Forschung und Entwicklung",
+         `2`="Finanzen, Versicherungen und Immobilien",
+         `3`="Lehre", 
+         `4`="Medizin (Pharma und Gesundheit",
+         `5`="Dienstleistungen und Handwerk",
+         `6`="Freizeit", 
+         `7`="Gesellschaft", 
+         `8`="Agrarwissenschaft", 
+         `9`="Technik", 
+         `10`="Sonstiges") %>% 
+  as.factor() -> raw.short$branch
 
 # Qualitätskontrolle 
 
