eaa9e49543a7776813c03ade0c8d07a29c9ca6cf
AnnaWolfram <anna.wolfram@rwth-aachen.de>
2023-07-31 19:03:29 +0200

    Noch eins

--- Daten/deskriptive_Statistik.R
+++ Daten/deskriptive_Statistik.R
@@ -1,36 +1,36 @@
 # Altersverteilung der Stichprobe
-summary(data$age)
+age_summary <- summary(data$age)
 
 # Geschlechterverteilung
-table(data$gender)
-prop.table(gender_summary) * 100
-round(gender_summary_percent, 2)
+gender_summary <- table(data$gender)
+gender_summary_percent <- prop.table(gender_summary) * 100
+gender_summary_percent <- round(gender_summary_percent, 2)
 
 # Bildungsabschluss
-table(data$education)
+education_summary <- table(data$education)
 
 # Beschäftigung
-table(data$occupation)
+occupation_summary <- table(data$occupation)
 
 # Wohnortgröße
-table(data$city_size)
+city_size_summary <- table(data$city_size)
 
 # Mobilitätsverhalten
 # Besitz Führerschein
-table(data$license)
+license_summary <- table(data$license)
 
 # Dauer Besitz Führerschein
-summary(data$duration_license)
+duration_license_summary <- summary(data$duration_license)
 
 # Auto Besitz (--> Art des Autos)
-table(data$car_type)
+car_type_summary <- table(data$car_type)
 
 # Fahrerfahrung (KM pro Jahr + Häufigkeit der Fahrertätigkeit)
-summary(data$km_per_year)
-table(data$frequency)
+km_per_year_summary <- summary(data$km_per_year)
+frequency_summary <- table(data$frequency)
 
 # Fahren alleine oder in Begleitung?
-table(data$companionship)
+companionship_summary <- table(data$companionship)
 
 # Präferenz der Fortbewegungsmittel?
 preferences_summary <- data %>%
@@ -38,4 +38,5 @@ preferences_summary <- data %>%
   gather(key = "question", value = "answer") %>%
   table()
 
-table(data$EV_Attitude)
+# Einstellung E-Mobilität
+ev_attitude_summary <- table(data$EV_Attitude)
