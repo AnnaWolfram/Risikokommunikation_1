ab9594273bd8ac61bd50d53971986a7ca9b639e0
AnnaWolfram <anna.wolfram@rwth-aachen.de>
2023-08-15 14:29:13 +0200

    Korrekturen

--- Daten/Vorwissen_Mann-Whitney-U-Test.R
+++ Daten/Vorwissen_Mann-Whitney-U-Test.R
@@ -6,10 +6,10 @@ library(stats)
 # Mapping des Vorwissens
 vorwissen_mapping <- c(
   "Ich könnte ihre Funktionsweise im Detail erklären." = 1,
-  "Ich habe eine relativ klare Vorstellung, wie E-Autos funktionieren." = 2,
-  "Ich habe eine ungefähre Vorstellung über die Funktionsweise von E-Autos." = 3,
-  "Ich habe schon einmal von E-Autos gehört, kann aber nichts darüber sagen." = 4,
-  "Ich habe keine Kenntnisse über Elektrofahrzeuge." = 5
+  "Ich habe eine relativ klare Vorstellung, wie E-Autos funktionieren." = 1,
+  "Ich habe eine ungefähre Vorstellung über die Funktionsweise von E-Autos." = 1,
+  "Ich habe schon einmal von E-Autos gehört, kann aber nichts darüber sagen." = 2,
+  "Ich habe keine Kenntnisse über Elektrofahrzeuge." = 2
 )
 
 # Anpassung der prior_knowledge_numeric Spalte
@@ -31,16 +31,26 @@ mwu_result <- wilcox.test(group1$Änderung_RW, group2$Änderung_RW)
 # Anzeigen der Ergebnisse
 print(mwu_result)
 
+# Robuste ANOVA mit oneway_test --------------------------------------
+# Lade das coin-Paket
+#install.packages("coin")
+library(coin)
 
-# Lade das WRS2-Paket
-library(WRS2)
+data_filtered$prior_knowledge_numeric <- as.factor(data_filtered$prior_knowledge_numeric)
+is.factor(data_filtered$prior_knowledge_numeric)
 
-# Führe eine robuste ANOVA durch
-robust_anova_result <- anovaBF(Änderung_RW ~ prior_knowledge_numeric, data = data_filtered, whichRandom = "ID")
+# Robuste ANOVA mit oneway_test
+robust_anova_one_way_result <- oneway_test(Änderung_RW ~ prior_knowledge_numeric, data = data_filtered)
+print(robust_anova_one_way_result)
 
+# Robuste ANOVA --------------------------------------
+# Lade das Paket lmrob
+#install.packages("robustbase")
+library(robustbase)
 
+# Robuste ANOVA
+robust_anova_result <- lmrob(Änderung_RW ~ prior_knowledge_numeric, data = data_filtered)
 
-
-
-
-
+# Zusammenfassung der Ergebnisse
+print(robust_anova_result)
+summary(robust_anova_result)
